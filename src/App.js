1946dc09a1012d22b0f541f609d411d42d7808c2
Meet Patel <patelmeet59@gmail.com>
2021-02-09 23:26:37 +0530

    Bootstrap the Project
    Signed-off-by: Meet Patel <patelmeet59@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Yosemite</h1>
+        <h3>Automatic Answer Checker</h3>
+        <h6>IT314 - Group 3</h6>
+      </header>
+    </div>
+  );
+}
+
+export default App;
