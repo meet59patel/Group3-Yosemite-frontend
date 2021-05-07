01df6b701808d2d0c29cc0457c97435b8d1cc24d
Meet Patel <patelmeet59@gmail.com>
2021-05-07 14:28:00 +0530

    hotfix: localhost
    Signed-off-by: Meet Patel <patelmeet59@gmail.com>

--- src/services/apis.service.js
+++ src/services/apis.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// const API = 'https://yosemite-sen.herokuapp.com';
-const API = 'http://localhost:8000';
+const API = 'https://yosemite-sen.herokuapp.com';
+// const API = 'http://localhost:8000';
 
 const userAPI = `${API}/users`;
 const assignmentAPI = `${API}/assignment`;
